6a8c7b935d3a905a604f3dc8a9cc890c7c986eb2
JobTracking <tuncalin@outlook.com>
2022-03-01 01:04:30 +0300

    TryGetValue() kullanımı(Değer varsa okur, yoksa set eder, geriye bool döner) , GetOrCreate metodu kullanım(Değer varsa okur, yoksa set eder, geriye datayı döner)

---
+++ RedisInMemory/Controllers/ProductController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedisInMemory.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IMemoryCache _memoryCache;
+        public ProductController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+        public IActionResult Index()
+        {
+            //Deger varsa olmaya çalışır yoksa değeri cache e set eder. 
+            if (!_memoryCache.TryGetValue("newsDate",out string newscache))
+            {
+                _memoryCache.Set<string>("newsDate", DateTime.Now.ToString());
+            }
+            //Değer varsa view e ekler
+            ViewBag.newsDate = newscache;
+
+            return View();
+        }
+        public IActionResult Show()
+        {
+            _memoryCache.Remove("newsDate"); //Siler
+
+
+            //Veriyi almaya çalışır alamaz ise veriyi oluşturup datayı geri döner..
+            _memoryCache.GetOrCreate<string>("newsDate", x => {
+                return DateTime.Now.ToString();
+            });
+
+            ViewBag.news= _memoryCache.Get<string>("newsDate");
+            return View();
+        }
+    }
+}
