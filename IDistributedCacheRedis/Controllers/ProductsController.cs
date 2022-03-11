d9656fda9ee5b82f0c0d48dee02ca77c073bafbb
JobTracking <tuncalin@outlook.com>
2022-03-11 14:42:49 +0300

    Microsoft.Extensions.Caching.StackExchangeRedis Kütüphanesinin eklenmesi, Servis eklenmesi, İmlementasyonunun yapılması

---
+++ IDistributedCacheRedis/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IDistributedCacheRedis.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private IDistributedCache _distributedCache;
+
+        public ProductsController(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
