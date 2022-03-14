5206aa2039bd8883196b05b78a0c0f316ff71bd1
JobTracking <tuncalin@outlook.com>
2022-03-14 23:30:22 +0300

    redisexchange string veri tipiyle işlemler set, get

---
+++ RedisExchangeAPI/Controllers/StringTypeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedisExchangeAPI.Services;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedisExchangeAPI.Controllers
+{
+    public class StringTypeController : Controller
+    {
+        private readonly RedisService _redisService;
+        private readonly IDatabase db;
+        public StringTypeController(RedisService redisService)
+        {
+            _redisService = redisService;
+            db = _redisService.GetDb(0);
+        }
+        public IActionResult Index()
+        {
+            //String Veri Tabanına Yazma
+            db.StringSet("name", "Adem Tunçalın");
+            db.StringSet("name", "Ali Veli");
+            return View();
+        }
+        public IActionResult Show()
+        {
+            var value= db.StringGet("name");
+            if (value.HasValue)
+            {
+                ViewBag.value = value.ToString();
+            }
+            return View();
+        }
+    }
+}
