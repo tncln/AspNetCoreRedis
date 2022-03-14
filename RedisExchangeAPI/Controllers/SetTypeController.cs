211477e1a8115071e144a315d867619076bc4a8e
JobTracking <tuncalin@outlook.com>
2022-03-15 01:35:29 +0300

    redis set hashset

---
+++ RedisExchangeAPI/Controllers/SetTypeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedisExchangeAPI.Services;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedisExchangeAPI.Controllers
+{
+    public class SetTypeController : Controller
+    {
+        private readonly RedisService _redisService;
+        private readonly IDatabase db;
+        public SetTypeController(RedisService redisService)
+        {
+            _redisService = redisService;
+            db = _redisService.GetDb(2);
+        }
+        public IActionResult Index()
+        {
+            HashSet<string> namesList = new HashSet<string>();
+            if (db.KeyExists("list"))
+            {
+                db.SetMembers("list").ToList().ForEach(x => {
+                    namesList.Add(x.ToString());
+                });
+            }
+            return View();
+        }
+        public IActionResult Add(string name)
+        {
+            db.KeyExpire("list", DateTime.Now.AddMinutes(5));
+            db.SetAdd("list", name);
+            return View();
+        }
+
+    }
+}
